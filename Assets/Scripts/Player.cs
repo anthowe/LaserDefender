dd8b1930706ee26c46f75c6dd01fca60ca61e1af
Anthony Howe <anthowe33@gmail.com>
2020-04-11 20:55:58 -0400

    Add Laser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
-   
+    [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-     
+
+        SetUpMoveBoundaries();
+        
         
     }
 
@@ -17,19 +28,40 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
-        
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+          GameObject laser =
+                Instantiate(laserPrefab,
+              transform.position,
+              Quaternion.identity) as GameObject;
+
+            laser.GetComponent<Rigidbody2D>().velocity = (new Vector2(0, projectileSpeed));
+        }
     }
 
     private void Move()
     {
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed ;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
       
-        var newXPos = transform.position.x + deltaX;
-        var newYPos = transform.position.y + deltaY;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX , xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
+    
+    }
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameGamera = Camera.main;
+        xMin = gameGamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameGamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
 
-       
+        yMin = gameGamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameGamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
