0593ef719348618f635456a0ccee57e9ff7c3764
anthowe <ahowe2@att.net>
2020-04-12 19:38:41 -0400

    Try add health/damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float damage = 10;
+   [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float  enemyProjectileSpeed = -0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyFire();
+    }
+     public void EnemyFire()
+    {
+        GameObject laser =
+           Instantiate(enemyLaserPrefab,
+         transform.position,
+         Quaternion.identity) as GameObject;
+
+        laser.GetComponent<Rigidbody2D>().velocity = (new Vector2(0, enemyProjectileSpeed)) * Time.deltaTime;
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,39 +9,81 @@ public class Player : MonoBehaviour
     [SerializeField] float padding = 0.5f;
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
+    BoxCollider2D myCollider;
     float xMin;
     float xMax;
     float yMin;
     float yMax;
+    float health = 10f;
+    float maxHealth = 100f;
 
-    
+   
     // Start is called before the first frame update
     void Start()
     {
 
         SetUpMoveBoundaries();
-        
-        
+        myCollider = FindObjectOfType<BoxCollider2D>();
+       
+       
     }
+
+  
 
     // Update is called once per frame
     void Update()
     {
         Move();
         Fire();
+        TakeDamage();
     }
 
     private void Fire()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-          GameObject laser =
-                Instantiate(laserPrefab,
-              transform.position,
-              Quaternion.identity) as GameObject;
+         firingCoroutine = StartCoroutine(FireContinuously());
+        }
+        if(Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while(true)
+        {
+            GameObject laser =
+            Instantiate(laserPrefab,
+          transform.position,
+          Quaternion.identity) as GameObject;
 
             laser.GetComponent<Rigidbody2D>().velocity = (new Vector2(0, projectileSpeed));
+            yield return new WaitForSeconds(projectileFiringPeriod);
         }
+ 
+    }
+
+ 
+
+
+
+    public void TakeDamage()
+    {
+        float currentHealth = maxHealth;
+        if (myCollider.IsTouchingLayers(LayerMask.GetMask("Projectile")))
+        {
+            currentHealth = maxHealth - health;
+            Debug.Log("Collision happened" + currentHealth);
+
+
+        }
+       
+       
     }
 
     private void Move()
@@ -49,8 +91,8 @@ public class Player : MonoBehaviour
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed ;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
       
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX , xMin, xMax);
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX , xMin + padding, xMax - padding);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin + padding, yMax - padding);
 
         transform.position = new Vector2(newXPos, newYPos);
     
@@ -58,10 +100,10 @@ public class Player : MonoBehaviour
     private void SetUpMoveBoundaries()
     {
         Camera gameGamera = Camera.main;
-        xMin = gameGamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameGamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        xMin = gameGamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameGamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
 
-        yMin = gameGamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameGamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+        yMin = gameGamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameGamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
     }
 }
