46c4f18db3d5601ef5f73f44c24085a318a29226
Anthony Howe <anthowe33@gmail.com>
2020-04-13 18:50:36 -0400

    Add Shredder

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        EnemyFire();
+        //EnemyFire();
     }
      public void EnemyFire()
     {
