8b21352685f464fda963e86dee143d9b26023dfc
anthowe <ahowe2@att.net>
2020-04-15 18:39:00 -0400

    Added Enemy lasers for real

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = .2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float enemyProjectileSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CountDownAndShoot();
+    }
 
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyLaser = Instantiate
+            (enemyLaserPrefab, 
+            transform.position, 
+            Quaternion.identity) as GameObject;
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = (new Vector2(0, -enemyProjectileSpeed));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
